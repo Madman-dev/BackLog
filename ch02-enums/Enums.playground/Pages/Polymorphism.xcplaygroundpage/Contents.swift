b66dd7bae54b3281125acece65a3162ae274a0aa
7과11사이 <dongjunlee222@gmail.com>
2023-12-14 23:30:05 +0900

    [ADD]: Polymorphism 완료

--- ch02-enums/Enums.playground/Pages/Polymorphism.xcplaygroundpage/Contents.swift
+++ ch02-enums/Enums.playground/Pages/Polymorphism.xcplaygroundpage/Contents.swift
@@ -2,6 +2,11 @@
 
 import Foundation
 
+/*
+ 배열은 순차적으로 데이터를 나열하지만 Any 타입을 담고 있을 경우
+ compile time에 어떤 데이터 타입인지 파악하기 쉽지 않음.
+ 타입에 대해 명시를 하는 방식은 개발자 입장에서 처리하거나 추가 개선을 할 수 있는 구조이기에 Any를 유지하지 않는 방향이 좋다.
+ */
 let arr: [Any] = [Date(), "Why was six afraid of seven?", "Because...", 789]
 
 for element in arr {
@@ -14,8 +19,7 @@ for element in arr {
     }
 }
 
-
-enum DateType {
+enum DateType { // two types of dates are available
     case singleDate(Date)
     case dateRange(Range<Date>)
 //    case year(Int8) // Uncomment to see compiler complaints
@@ -33,6 +37,25 @@ for dateType in dates {
     switch dateType {
     case .singleDate(let date): print("Date is \(date)")
     case .dateRange(let range): print("Range is \(range)")
+    }
+}
+
+// _______________ Practice
+
+enum NoType {
+    case singleData(Date)
+    case multiData(Range<Date>)
+}
+
+let data: [NoType] = [
+    NoType.singleData(Date.now),
+    NoType.multiData(Date.now..<Date(timeIntervalSinceNow: 3600))
+]
+
+for microData in data {
+    switch microData {
+    case .singleData(let newData): print("이러네요 \(newData)")
+    case .multiData(let forward): print("어머나 \(forward)")
     }
 }
 
