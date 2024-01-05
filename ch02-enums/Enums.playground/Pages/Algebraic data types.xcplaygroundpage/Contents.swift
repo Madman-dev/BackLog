1ade3341c8b81060767f4ab6dc10d41f02cbfd9d
7과11사이 <dongjunlee222@gmail.com>
2024-01-05 15:42:29 +0900

    [ADD]: Algebraic data type 정리 중

--- ch02-enums/Enums.playground/Pages/Algebraic data types.xcplaygroundpage/Contents.swift
+++ ch02-enums/Enums.playground/Pages/Algebraic data types.xcplaygroundpage/Contents.swift
@@ -2,6 +2,33 @@
 
 //: # Algebraic data types
 
+/*
+ - Enums are algebraic data types.
+ 
+ Algebraic data types are types of 'composed data'.
+ They include sum type and product type.
+ 
+ - Sum Type은 '오로지 한 가지' 값인 것
+ - Product Type은 '여러가지' 값이 될 수 있는 것
+ 👉🏻 그렇다면 앞서 이야기들을 돌아보면 구조체 == product type!
+ */
+
+//MARK: - Sum & Product Type
+// id와 name - 책을 구분 지을 수 있는 두 가지 값이 담기면 PRODUCT
+struct Books {
+    let id: UUID
+    let name: String
+    let page: Int
+}
+
+// 책의 상황을 케이스별로 나누어 "한가지의 상태만 확인" - thus this is a SUM
+enum Book {
+    // 각 케이스별로 책의 상태를 확인하게 된다.
+    case read
+    case unread
+}
+
+
 import Foundation
 
 enum Day {
