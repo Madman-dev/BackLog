ff9728d32ae7a12f0e3c5df7dc523051c2662e89
7과11사이 <dongjunlee222@gmail.com>
2024-01-08 12:45:46 +0900

    [ADD]: Safer Use of Strings

--- Books/SwiftInDepth/ch02-enums/Enums.playground/Pages/Raw value enum.xcplaygroundpage/Contents.swift
+++ Books/SwiftInDepth/ch02-enums/Enums.playground/Pages/Raw value enum.xcplaygroundpage/Contents.swift
@@ -4,17 +4,38 @@
 
 import Foundation
 
-enum Currency: String {
-    case euro = "euro"
-    case usd = "usd"
-    case gbp = "gdp"
+/*
+ enums with raw values are defined at compile time where as with associated types store values at runtime.
+ Giving a direct value would allow the enum to figure out the data's Type during compile time.
+ */
+
+//enum Currency: String {
+//    case euro = "euro"
+//    case usd = "usd"
+//    case gbp = "gdp"
+//}
+
+enum Currency: String {    // no need to give value if types are single.
+    case euro = "eur"      // by giving a raw value to an enum, possible wrong typing issue rises
+    case usd
+    case gbp
 }
 
 let currency = Currency.euro
-print(currency.rawValue) // "usd"
+print(currency.rawValue)
 
-
+// API 콜을 하는 상황에서 rawValue가 틀릴 경우를 가정해보자
 let parameters = ["filter": currency.rawValue]
 print(parameters) // ["filter": "euro"]
+
+
+// 차라리 타입을 지정해서 안전하게 생성할 수도 있다.
+let parameter: [String: String]
+
+switch currency {
+case .euro: parameter = ["유럽": "euro"]
+case .gbp: parameter = ["영국": "gdp"]
+case .usd: parameter = ["미국": "usd"]
+}
 
 //: [Table of contents](Table%20of%20contents) - [Previous page](@previous) - [Next page](@next)
