689d2523410fb7747e01244782b66df16ffaba7b
7과11사이 <dongjunlee222@gmail.com>
2024-01-20 01:07:04 +0900

    [iOS]: Dependency Injection sample Update

--- Becoming iOS Dev/DependencyInjection/Dependency Injection.playground/Pages/MyPlayground.xcplaygroundpage/Contents.swift
+++ Becoming iOS Dev/DependencyInjection/Dependency Injection.playground/Pages/MyPlayground.xcplaygroundpage/Contents.swift
@@ -26,19 +26,76 @@ import UIKit
  ➡ 이미 잘 알고 있듯이, Dependency Injection은 클래스를 분리(decoupling)하거나 코드를 유연하게 만들 수 있는 방법인 셈.
  */
 
-
-class M {
-    
+// before injection
+class CreditCardMethod {
+    func processPayment(amount: Double) {
+        print("\(amount) 비용이 발생합니다.")
+    }
 }
 
-class N {
-    var name: String
+class PaypalMethod {
+    func processPayment(amount: Double) {
+        print("\(amount) 비용이 발생합니다.")
+    }
+}
+
+class PaymentProcessor {
+    let creditCard: CreditCardMethod
+    let paypal: PaypalMethod
     
-    init(name: String) {
-        self.name = name
+    init() {
+        self.creditCard = CreditCardMethod()
+        self.paypal = PaypalMethod()
     }
     
-    func printName() {
-        print(self.name)
-    } 
+    func processCreditCardPayment(amount: Double) {
+        creditCard.processPayment(amount: amount)
+    }
+    
+    func processPaypalPayment(amount: Double) {
+        paypal.processPayment(amount: amount)
+    }
 }
+
+
+// after injection
+protocol InjectedGateway {
+    func pay(amount: Double)
+}
+
+class InjectCreditCardPayment: InjectedGateway {
+    func pay(amount: Double) {
+        print("이제 카드로 \(amount) 비용을 지출합니다.")
+    }
+}
+
+class InjectPaypalPayment: InjectedGateway {
+    func pay(amount: Double) {
+        print("지금은 페이팔로 \(amount) 비용을 지출합니다.")
+    }
+}
+
+// InjectedPaymentProcessor now depends on interface and protocol - high level abstraction
+class InjectPaymentProcessor {
+    let payment: InjectedGateway
+    
+    init(paymentGateway: InjectedGateway) {
+        self.payment = paymentGateway
+    }
+    
+    func pay(amount: Double) {
+        payment.pay(amount: amount)
+    }
+}
+
+/*
+ 항상 같은 흐름을 타고 있지만, 이해는 되는 것 같다.
+ 코드를 변형할 수 있다는 점이 어떤 점인지 이해가 간다.
+ 프로젝트를 할 때 프로토콜로 의존성을 분리한 경험이 있는데, 간단했기에 큰 문제 없이 잘 적용하긴 했다.
+ 
+ 하지만 명확하게 아직 이해하지 못하는 부분은 실제 프로젝트를 할 때 어떻게 분리가 될지 감이 안오기는 한다.
+ 느끼기로는 프로토콜과 하나의 클래스만으로 처리할 수 있는 일을 굳이 여러 클래스를 만들어서 할 필요가 없고 아름답지 못하다인데,
+ 제대로 사용해보면서 조금 더 이해력을 쌓아보아야겠다.
+ */
+
+// 참고: https://mrezkys.medium.com/dependency-inversion-principle-d-in-solid-theory-3ad2a409df4a
